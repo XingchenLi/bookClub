a47ad13d3fdb6f9ab30aaeb9526281429a1c8397
Xingchen Li <lix17@miamioh.edu>
2014-12-18 02:08:04 -0500

    add bookClub Website to the GIthub

---
+++ bookclub/Site/user-profile.php
@@ -0,0 +1,501 @@
+<!DOCTYPE HTML>
+<html lang="zh-C" class="ua-mac ua-webkit">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="bookClub" />
+<meta name="description" content="Our cheap textbook solution" />
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" rel="stylesheet" />
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+</head>
+
+
+
+
+
+<body>
+
+
+
+
+
+
+
+
+
+
+
+
+  <div id="header-wrapper" >
+  <div id="header" class="container">
+    <div id = "Menu">
+    <ul>
+    <li> <a target="_blank" class="lnk-book" href=*>my bookshelves </a></li>
+    <li> <a target="_blank" class="lnk-circle" href="*">my circle</a></li>
+    <li> <a target="_blank" class="lnk-news" href="*">school news </a></li>
+    <li> <a target="_blank" class="lnk-activities" href="*">activities</a></li>
+    <li> <a target="_blank" class="lnk-club" href="*">my c</a></li>
+    <li><a target="_blank" class="lnk-club" href="*">lub</a></li>
+    </ul>
+    </div>
+    </div>
+    <h1><a href="login-index">BookClub</a></h1>
+
+    <div class="anony-srh">
+    <form action="http://www.douban.com/search" method="get">
+      <span class="inp"><input type="text" maxlength="60" size="12" placeholder="good day" name="q" autocomplete="off"></span>
+    <span class="bn"><input type="submit" value="Search"></span>
+    </form>
+    </div>
+  </div>
+
+
+
+<div id="anony-reg">
+<div class="wrapper">
+    
+
+<div class="login">
+    <form id="lzform" name="lzform" method="post" action="http://www.douban.com/accounts/login">
+        <fieldset>
+            <legend>Log In</legend>
+            <input type="hidden" value="index_nav" name="source">
+            <div class="item item-account">
+                <input type="text" name="form_email" id="form_email" value="" class="inp" placeholder="Email Address" tabIndex="1">
+                <div class="opt"><label for="form_remember"><input name="remember" type="checkbox" id="form_remember" tabIndex="4">Remember me</label></div>
+            </div>
+            <div class="item item-passwd">
+                <input name="form_password" placeholder="Pawwsord" id="form_password" class="inp" type="password" tabIndex="2">
+                <div class="opt"><a href="http://www.douban.com/accounts/resetpassword">Forget Password</a></div>
+            </div>
+            <div class="item-submit">
+                <input value="Log In" type="submit" class="bn-submit" tabIndex="4">
+            </div>
+        </fieldset>
+    </form>
+    
+  
+    <div class="reg">
+        
+        <a href="/accounts/register" class="lnk-reg">Join us <i>Sign up</i></a>
+        <div class="nb-info">
+        <b>Find more about our book </b>
+        </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+            
+<div id="anony-sns" class="section">
+  <div class="wrapper">
+  
+<!-- douban ad begin -->
+<div id="dale_anonymous_homepage_top_for_crazy_ad"></div>
+<!-- douban ad end -->
+
+  
+  <div class="side">
+
+  <div style="margin:10px 0px;"></div>
+  
+    <h2>
+        Deal of the day
+            &nbsp;&middot;&nbsp;&middot;&nbsp;&middot;&nbsp;&middot;&nbsp;&middot;&nbsp;&middot;
+        <span class="pl">&nbsp;(
+                
+                    <a href="http://www.douban.com/online/?r=i" target="_self">more</a>
+                ) </span>
+    </h2>
+
+  <div class="bookDeal">
+    <ul>
+        <li>
+            <div class="bookTitle">
+                <a href=*>Math for dummy</a>
+            </div>
+            <div class="desc">
+               Price:
+               
+            </div>
+        </li>
+        <li>
+            <div class="title">
+                <a href="http://www.douban.com/online/12023255/">Effective Java</a>
+            </div>
+            <div class="desc">
+               Price:
+            </div>
+        </li>
+        <li>
+            <div class="title">
+                <a href="http://www.douban.com/online/12012907/">Filthy Rich Client</a>
+            </div>
+            <div class="desc">Price:</div>
+        </li>
+        <!-- douban ad begin -->
+        <li>
+          <div id="dale_homepage_online_activity_promo_1"></div>
+        </li>
+        <li>
+          <div id="dale_anonymous_homepage_doublemint"></div>
+        </li>
+        <!-- douban ad end -->
+    </ul>
+  </div>
+</div>
+  <div class="main">
+<div class="mod">
+
+  
+    <h2>
+        Hot Topic
+            &nbsp;&middot;&nbsp;&middot;&nbsp;&middot;&nbsp;&middot;&nbsp;&middot;&nbsp;&middot;
+    </h2>
+
+  <div class="topic">
+    <ul>
+      <li>
+      <div class="pic">
+          <a href="http://www.douban.com/photos/album/74784358/"><img src="http://img3.douban.com/pics/blank.gif" data-origin="http://img5.douban.com/view/photo/albumcover/public/p1918178246.jpg" alt="" /></a>
+      </div>
+      BlaBla<li>
+      <div class="pic">
+          <a href="http://www.douban.com/photos/album/32009276/"><img src="http://img3.douban.com/pics/blank.gif" data-origin="http://img3.douban.com/view/photo/albumcover/public/p1949544191.jpg" alt="" /></a>
+      </div>
+      Good stuff<li>
+      <div class="pic">
+          <a href="http://www.douban.com/photos/album/145351223/"><img src="http://img3.douban.com/pics/blank.gif" data-origin="http://img5.douban.com/view/photo/albumcover/public/p2207858946.jpg" alt="" /></a>
+      </div>
+      Hot girl got naked <li>
+      <div class="pic">
+          <a href="http://www.douban.com/photos/album/149223212/"><img src="http://img3.douban.com/pics/blank.gif" data-origin="http://img3.douban.com/view/photo/albumcover/public/p2216085665.jpg" alt="" /></a>
+      </div>
+      xingchen li is awesome</ul>
+  </div>
+  <div class="notes">
+    <ul>
+      <li class="first">what should i go to make it possible 
+        <div class="author">
+        book author 
+      </div>
+      
+      </li>
+
+      <li><a href=*>国内最适合坐火车看风</a></li>
+      <li><a href="http://www.douban.com/note/379938685/">有毅力的人无论怎么样都会过的很幸福</a></li>
+      <li><a href="http://www.douban.com/note/469575163/">三十年后的少女心</a></li>
+      <li><a href="http://www.douban.com/note/470598056/">生娃如跳楼，你可想好了再往下蹦</a></li>
+      <li><a href="http://www.douban.com/note/469169990/">不管这世界已经告诉你什么，未发生的总值得一试</a></li>
+      <li><a href="http://www.douban.com/note/467962769/">煲汤的时候，人会显得特别温柔</a></li>
+      <li><a href="http://www.douban.com/note/470677345/">我出书了，第一本书</a></li>
+      <li><a href="http://www.douban.com/note/464882878/">零售高手必备的枕边书单</a></li>
+      <li><a href="http://www.douban.com/note/468789658/">你不用回来</a></li>
+    </ul>
+  </div>
+</div>
+</div>
+  </div>
+  
+<div id="dale_anonymous_home_page_top" class="extra"></div>
+
+</div>
+
+
+
+
+
+
+
+
+            
+<div id="anony-dongxi" class="section">
+  <div id="dale_anonymous_home_page_middle" class="extra"></div>
+
+</div>
+<div id="anony-book" class="section">
+  <div class="wrapper">
+  
+  
+<div class="sidenav">
+  <div class="mod">
+  <h2 class="section-title"><a href="http://book.douban.com">Friends Reading</a></h2>
+  
+  
+  <div class="side-links nav-anon">
+      <ul>
+                                                    
+                 <li><a href="http://book.douban.com/tag/">Recent read</a></li>
+             
+                <li>
+                    <a target="_blank" href="http://read.douban.com?dcn=entry&amp;dcs=book-nav&amp;dcm=douban">Friend's note</a>
+                    <img style="top: 4px; position: absolute;" src="http://img3.douban.com/pics/new_menu.gif"/>
+                </li>
+             
+                 <li><a href="http://book.douban.com/writers/">Author </a></li>
+             
+                 <li><a href="http://book.douban.com/review/best/">书评</a></li>
+      
+          
+<li class="site-nav-prom">
+  <a class="lnk-buy" href="http://book.douban.com/cart"><em>购书单</em></a>
+</li>
+
+      </ul>
+  </div>
+ 
+  </div>
+
+  
+
+
+    <div class="apps-list">
+    <ul>
+    <li><a href="http://www.douban.com/mobile/read" class="lnk-icon"><i class="app-icon app-icon-read"></i></a>Book club reading<a href="http://www.douban.com/mobile/read"></a></li>
+    <li><a href="http://www.douban.com/mobile/read-notes" class="lnk-icon"><i class="app-icon app-icon-read-notes"></i></a><a href="http://www.douban.com/mobile/read-notes">BookClub recommend </a></li>
+    <li><a href="http://www.douban.com/mobile/cart" class="lnk-icon"><i class="app-icon app-icon-cart"></i></a><a href="http://www.douban.com/mobile/cart">BookClub buying list </a></li>
+    </ul>
+    </div>
+
+</div>
+
+  <div class="side">
+
+<div class="mod">
+
+    <h2>
+        Hot Tag
+            &nbsp;&middot;&nbsp;&middot;&nbsp;&middot;&nbsp;&middot;&nbsp;&middot;&nbsp;&middot;
+            <span class="pl">&nbsp;(
+                
+                    <a href="http://book.douban.com/tag/?view=type" target="_self">More</a>
+                ) </span>
+    </h2>
+
+<div class="book-cate-mod">
+<div class="cate book-cate">
+  <ul>
+  <li class="cate-label">[文学]</li>
+  <li><a href="http://book.douban.com/tag/小说">小说</a></li>
+  <li><a href="http://book.douban.com/tag/随笔">随笔</a></li>
+  <li><a href="http://book.douban.com/tag/散文">散文</a></li>
+  <li><a href="http://book.douban.com/tag/日本文学">日本文学</a></li>
+  <li><a href="http://book.douban.com/tag/童话">童话</a></li>
+  <li><a href="http://book.douban.com/tag/诗歌">诗歌</a></li>
+  <li><a href="http://book.douban.com/tag/名著">名著</a></li>
+  <li><a href="http://book.douban.com/tag/港台">港台</a></li>
+  <li><a href="http://book.douban.com/tag/?view=type#文学">更多</a></li>
+  </ul>
+</div>
+<div class="cate book-cate">
+  <ul>
+  <li class="cate-label">[流行]</li>
+  <li><a href="http://book.douban.com/tag/漫画">漫画</a></li>
+  <li><a href="http://book.douban.com/tag/绘本">绘本</a></li>
+  <li><a href="http://book.douban.com/tag/推理">推理</a></li>
+  <li><a href="http://book.douban.com/tag/青春">青春</a></li>
+  <li><a href="http://book.douban.com/tag/言情">言情</a></li>
+  <li><a href="http://book.douban.com/tag/科幻">科幻</a></li>
+  <li><a href="http://book.douban.com/tag/武侠">武侠</a></li>
+  <li><a href="http://book.douban.com/tag/奇幻">奇幻</a></li>
+  <li><a href="http://book.douban.com/tag/?view=type#流行">更多</a></li>
+  </ul>
+</div>
+<div class="cate book-cate">
+  <ul>
+  <li class="cate-label">[文化]</li>
+  <li><a href="http://book.douban.com/tag/历史">历史</a></li>
+  <li><a href="http://book.douban.com/tag/哲学">哲学</a></li>
+  <li><a href="http://book.douban.com/tag/传记">传记</a></li>
+  <li><a href="http://book.douban.com/tag/设计">设计</a></li>
+  <li><a href="http://book.douban.com/tag/建筑">建筑</a></li>
+  <li><a href="http://book.douban.com/tag/电影">电影</a></li>
+  <li><a href="http://book.douban.com/tag/回忆录">回忆录</a></li>
+  <li><a href="http://book.douban.com/tag/音乐">音乐</a></li>
+  <li><a href="http://book.douban.com/tag/?view=type#文化">更多</a></li>
+  </ul>
+</div>
+<div class="cate book-cate">
+  <ul>
+  <li class="cate-label">[生活]</li>
+  <li><a href="http://book.douban.com/tag/旅行">旅行</a></li>
+  <li><a href="http://book.douban.com/tag/励志">励志</a></li>
+  <li><a href="http://book.douban.com/tag/职场">职场</a></li>
+  <li><a href="http://book.douban.com/tag/美食">美食</a></li>
+  <li><a href="http://book.douban.com/tag/教育">教育</a></li>
+  <li><a href="http://book.douban.com/tag/灵修">灵修</a></li>
+  <li><a href="http://book.douban.com/tag/健康">健康</a></li>
+  <li><a href="http://book.douban.com/tag/家居">家居</a></li>
+  <li><a href="http://book.douban.com/tag/?view=type#生活">更多</a></li>
+  </ul>
+</div>
+<div class="cate book-cate">
+  <ul>
+  <li class="cate-label">[经管]</li>
+  <li><a href="http://book.douban.com/tag/经济学">经济学</a></li>
+  <li><a href="http://book.douban.com/tag/管理">管理</a></li>
+  <li><a href="http://book.douban.com/tag/金融">金融</a></li>
+  <li><a href="http://book.douban.com/tag/商业">商业</a></li>
+  <li><a href="http://book.douban.com/tag/营销">营销</a></li>
+  <li><a href="http://book.douban.com/tag/理财">理财</a></li>
+  <li><a href="http://book.douban.com/tag/股票">股票</a></li>
+  <li><a href="http://book.douban.com/tag/企业史">企业史</a></li>
+  <li><a href="http://book.douban.com/tag/?view=type#经管">更多</a></li>
+  </ul>
+</div>
+<div class="cate book-cate">
+  <ul>
+  <li class="cate-label">[科技]</li>
+  <li><a href="http://book.douban.com/tag/科普">科普</a></li>
+  <li><a href="http://book.douban.com/tag/互联网">互联网</a></li>
+  <li><a href="http://book.douban.com/tag/编程">编程</a></li>
+  <li><a href="http://book.douban.com/tag/交互设计">交互设计</a></li>
+  <li><a href="http://book.douban.com/tag/算法">算法</a></li>
+  <li><a href="http://book.douban.com/tag/通信">通信</a></li>
+  <li><a href="http://book.douban.com/tag/神经网络">神经网络</a></li>
+  <li><a href="http://book.douban.com/tag/?view=type#科技">更多</a></li>
+  </ul>
+</div>
+</div>
+</div>
+</div>
+  </div>
+  
+</div>
+<div class="wrapper">
+  <div id="dale_anonymous_home_page_bottom" class="extra"></div>
+  <div id="ft"></div>
+</div>
+
+
+
+
+
+
+<script src="http://img3.douban.com/f/shire/75a96a9c050c54de9110333d66ddfba490650830/js/core/do/_init_.js" data-cfg-corelib="http://img3.douban.com/f/shire/8033e1c2528bb6402c478e00176afb3562abbc21/js/jquery.min.js"></script>
+<script type="text/javascript" src="http://img3.douban.com/misc/mixed_static/4ab298486326ad89.js"></script>
+
+    <!-- douban ad begin -->
+    
+
+
+
+
+    
+<script type="text/javascript">
+    (function (global) {
+        var newNode = global.document.createElement('script'),
+            existingNode = global.document.getElementsByTagName('script')[0],
+            adSource = 'http://erebor.douban.com/',
+            userId = '',
+            browserId = 'oqhF4BcamYg',
+            ipAddress = '50.30.132.193',
+            criteria = '3:/',
+            preview = '',
+            debug = false,
+            adSlots = ['dale_anonymous_homepage_top_for_crazy_ad', 'dale_anonymous_homepage_movie_bottom', 'dale_anonymous_home_page_top', 'dale_homepage_online_activity_promo_1', 'dale_anonymous_homepage_doublemint', 'dale_anonymous_home_page_middle', 'dale_anonymous_home_page_middle_2', 'dale_anonymous_home_page_bottom'];
+
+        global.DoubanAdRequest = {src: adSource, uid: userId, bid: browserId, ip: ipAddress, crtr: criteria, prv: preview, debug: debug};
+        global.DoubanAdSlots = (global.DoubanAdSlots || []).concat(adSlots);
+
+        newNode.setAttribute('type', 'text/javascript');
+        newNode.setAttribute('src', 'http://img3.douban.com/f/shire/9a05c5455e63a8174be4fc85e713885c2a521c2a/js/ad.js');
+        newNode.setAttribute('async', true);
+        existingNode.parentNode.insertBefore(newNode, existingNode);
+    })(this);
+</script>
+
+
+
+
+
+
+
+
+
+
+    <!-- douban ad end -->
+    
+
+
+
+
+
+<script type="text/javascript">
+var _paq = _paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+    var p=(('https:' == document.location.protocol) ? 'https' : 'http'), u=p+'://fundin.douban.com/';
+    _paq.push(['setTrackerUrl', u+'piwik']);
+    _paq.push(['setSiteId', '100001']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; 
+    g.type='text/javascript';
+    g.defer=true; 
+    g.async=true; 
+    g.src=p+'://s.doubanio.com/dae/fundin/piwik.js';
+    s.parentNode.insertBefore(g,s);
+})();
+</script>
+
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-7019765-1']);
+_gaq.push(['_addOrganic', 'baidu', 'word']);
+_gaq.push(['_addOrganic', 'soso', 'w']);
+_gaq.push(['_addOrganic', '3721', 'name']);
+_gaq.push(['_addOrganic', 'youdao', 'q']);
+_gaq.push(['_addOrganic', 'so.360.cn', 'q']);
+_gaq.push(['_addOrganic', 'vnet', 'kw']);
+_gaq.push(['_addOrganic', 'sogou', 'query']);
+_gaq.push(['_addIgnoredOrganic', '豆瓣']);
+_gaq.push(['_addIgnoredOrganic', 'douban']);
+_gaq.push(['_addIgnoredOrganic', '豆瓣网']);
+_gaq.push(['_addIgnoredOrganic', 'www.douban.com']);
+_gaq.push(['_setDomainName', '.douban.com']);
+
+
+    _gaq.push(['_setCustomVar', 1, 'responsive_view_mode', 'desktop', 3]); 
+
+_gaq.push(['_trackPageview']);
+_gaq.push(['_trackPageLoadTime']);
+
+window._ga_init = function() {
+    var ga = document.createElement('script');
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    ga.setAttribute('async', 'true');
+    document.documentElement.firstChild.appendChild(ga);
+};
+if (window.addEventListener) {
+    window.addEventListener('load', _ga_init, false);
+} else {
+    window.attachEvent('onload', _ga_init);
+}
+</script>
+
+
+
+
+
+
+
+<script>var _check_hijack = function () {
+    var _sig = "oqhF4Bca", _login = false, bid = get_cookie('bid');
+    if (location.protocol != "file:" && (
+            typeof(bid) != "string" && _login ||
+            typeof(bid) == "string" && bid.substring(0,8) != _sig)) {
+        location.href += (/\?/.test(location.href)?"&":"?") + (
+                "_r=" + Math.random().toString(16).substring(2));
+    }};
+if (typeof(Do) != 'undefined') Do(_check_hijack);
+else if (typeof(get_cookie) != 'undefined') _check_hijack();
+</script></body>
+</html>
